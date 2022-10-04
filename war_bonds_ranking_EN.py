8b2b368ffbe2eca9b7f946bd156417189e3678ff
datorymen <87224912+datorymen@users.noreply.github.com>
2022-10-04 15:01:27 +0800

    Add files via upload

--- war_bonds_ranking_EN.py
+++ war_bonds_ranking_EN.py
@@ -33,8 +33,8 @@ st.write('Updated：' + now_str)
 st.write('Note：This free data source is not updated immediately. Suggest to refresh every 2-3 hours.')
 
 st.caption('''
-Any feedback or suggestion，please message me: \n
-Discord: datory.men#9568 \n
+Donation is welcome to keep the app running. Thank you! \n
+0x7E71DdaDF7b1D998eECEE8D99817375B84110C13 \n
 ''')
 
 scraper = cloudscraper.create_scraper(
